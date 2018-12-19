9a5b775a1c37204fbecbeefab9289d9422364c02
Tetsuya Kitabayashi <tetuya01465@gmail.com>
2018-12-19 13:57:23 +0900

    add karabiner and update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+set -u
+cd ~/work/dotfiles
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+git submodule init
+git submodule update
+
+echo "start setup..."
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    #[[ "$f" = ".gitconfig.local.template" ]] && continue
+    #[[ "$f" = ".gitmodules" ]] && continue
 
-    echo "$f"
+    ln -snfv ~/dotfiles/"$f" ~/
 done
 
